70ab98d8b261fa31e6928b101c4f02d56aa412c4
Philip Pretorius <philippretorius.79@gmail.com>
2023-06-15 19:51:10 +0200

    Ship object test added

--- ship.test.js
+++ ship.test.js
@@ -1,35 +1,13 @@
 const Ship = require('./ship');
 
-/* test('Create a ship with length 4, hit 0, not sunk', () => {
-  expect(Ship(4)).toEqual({
-    length: 4,
-    hits: 0,
-    sunk: false,
-    hit:   function hit() {
-      this.hits += 1;
-      this.sunk = this.isSunk();
-    },
-    isSunk: function isSunk() {
-      return this.hits === this.length;
-    },
-  })
-}); */
-
 test('Ship type = function', () => {
   expect(typeof(Ship)).toBe('function');
 })
 
-/* test('Create a new ship with a length 3, hit 0, not sunk', () => {
+test('Ship1 type = object', () => {
   const ship1 = Ship(3);
-  expect(ship1).toEqual({
-    length: 3,
-    hits: 0,
-    sunk: false,
-    hit: hit(),
-    isSunk: isSunk(),
-  })
-});
- */
+  expect(typeof(ship1)).toBe('object');
+})
 
 test('Ship1 is hit 3 times', () => {
   const ship1 = Ship(3);

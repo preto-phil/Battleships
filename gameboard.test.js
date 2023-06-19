de483b91c6c2e0f4b9cf6731cf97df5448854d6d
Philip Pretorius <philippretorius.79@gmail.com>
2023-06-20 00:32:00 +0200

    Test for ship placement passed

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,17 +1,20 @@
 const Gameboard = require('./gameboard');
+const Ship = require('./ship');
+
 
 test('Gameboard size = 100', () => {
   const gameboard = Gameboard();
   expect(gameboard.gameBoard.length).toBe(100);
 });
 
-/* 
+
 test('Ship at row 5 and col 0', () => {
-  const carrier = Ship(5);
+  const carrier = Ship('carrier', 5);
   const battleship = Ship(4);
   const destroyer = Ship(3);
   const submarine = Ship(3);
   const patrolBoat = Ship(2);
-  const car = Gameboard.placeShip(carrier, 5, 0);
-  expect(car.gameBoard[50]).toBe({ row: 5, col: 0, ship});
-}); */
+  const carrierPlaced = Gameboard()
+  carrierPlaced.placeShip(carrier, 5, 0);
+  expect(carrierPlaced.gameBoard[50]).toEqual({ row: 5, col: 0, ship: 'carrier' });
+});
